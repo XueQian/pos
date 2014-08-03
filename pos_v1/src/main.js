c500405549603bf42d41bcb711d18cdde15e9c6f
XueQian <xueqian0321@126.com>
2014-08-03 22:19:00 +0800

    Update main.js

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -52,8 +52,6 @@ function printInventory(inputs){
   var totalPromotion = 0;
   for (var i = 0; i < actualPay.length; i++){
     totalPromotion += actualPay[i]; }
-  expectText += '总计：'+totalPromotion.toFixed(2)+'(元)\n';
-  expectText += '节省：'+totalPay.toFixed(2)+'(元)\n'
-  expectText += '**********************';
+  expectText += '总计：'+totalPromotion.toFixed(2)+'(元)\n'+'节省：'+totalPay.toFixed(2)+'(元)\n'+'**********************';s
   console.log(expectText);
   }
